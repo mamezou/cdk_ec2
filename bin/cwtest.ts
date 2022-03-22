601c4623124e11a9f3745d1c96efc0eeb4d51074
mamezou <mamezou.king@gmail.com>
2022-03-22 22:44:44 +0900

    first commit

--- bin/cwtest.ts
+++ bin/cwtest.ts
@@ -3,4 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import { CwtestStack } from '../lib/cwtest-stack';
 
 const app = new cdk.App();
-new CwtestStack(app, 'CwtestStack');
+new CwtestStack(app, 'CwtestStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
--- lib/cwtest-stack.ts
+++ lib/cwtest-stack.ts
@@ -1,19 +1,96 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
+import * as cw_actions from 'aws-cdk-lib/aws-cloudwatch-actions';
 import { Construct } from 'constructs';
 
 export class CwtestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CwtestQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // create new VPC
+    const vpc = new ec2.Vpc(this, 'VPC');
+
+    // use default VPC
+    // const vpc = ec2.Vpc.fromLookup(this, 'VPC', { isDefault: true });
+
+    // create new security group
+    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'Security group for the test',
+      securityGroupName: 'TestSecurityGroup',
     });
 
-    const topic = new sns.Topic(this, 'CwtestTopic');
+    // allow all traffic
+    securityGroup.connections.allowToAnyIpv4(ec2.Port.tcp(80));
+    securityGroup.connections.allowToAnyIpv4(ec2.Port.tcp(443));
+    securityGroup.connections.allowToAnyIpv4(ec2.Port.tcp(8080));
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // allow ssh access
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'SSH');
+
+    // create new instance
+    const instance = new ec2.Instance(this, 'Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(
+        { generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }
+      ),
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      securityGroup,
+
+      // preinstalled software
+      init: ec2.CloudFormationInit.fromConfigSets({
+        configSets: {
+          default: ['yumPreinstall'],
+        },
+        configs: {
+          yumPreinstall: new ec2.InitConfig([
+            ec2.InitPackage.yum('git'),
+          ])
+        }
+
+      })
+    });
+
+    // create new cloudwatch metric
+    const metric = new cloudwatch.Metric({
+      namespace: 'AWS/EC2',
+      metricName: 'CPUUtilization',
+      period: Duration.minutes(5),
+    });
+
+    // add alarm
+    // const alarm = new cloudwatch.Alarm(this, 'Alarm', {
+    //   metric,
+    //   threshold: 50,
+    //   evaluationPeriods: 1,
+    //   alarmDescription: 'Alarm when CPU is above 50%',
+    //   actionsEnabled: true,
+    //   alarmActions: [
+    //     new cw_actions.SnsAction(this, 'SnsAction', {
+    //       topic: new cw_actions.SnsTopic(this, 'SnsTopic', {
+    //         displayName: 'AlarmTopic',
+    //       }),
+    //     }),
+    //   ],
+    // });
+
+
+    // create new cloudwatch alarm
+    // const alarm = new cloudwatch.Alarm(this, 'Alarm', {
+    //   metric,
+    //   threshold: 80,
+    //   evaluationPeriods: 1,
+    //   treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+    // });
+
+    // todo : add cpu alarm from cloudwatch to sns
+
+
+    new CfnOutput(this, 'ec2-instance-id', {
+      value: instance.instanceId,
+    });
+
   }
 }
